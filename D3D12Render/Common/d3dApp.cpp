b740dd265ffaaef22435b78f2088262af7d6f878
MythGalaxy <405503818@qq.com>
2020-03-25 18:24:27 +0800

    Implement d3dApp

--- D3D12Render/Common/d3dApp.cpp
+++ D3D12Render/Common/d3dApp.cpp
@@ -1,2 +1,193 @@
 ﻿
 #include "d3dApp.h"
+
+D3DApp* D3DApp::mApp = nullptr;
+D3DApp::D3DApp(HINSTANCE hInstance) :mhAppInst(hInstance)
+{
+    assert(mApp == nullptr);
+    mApp = this;
+}
+
+D3DApp::D3DApp()
+{
+    assert(mApp == nullptr);
+    mApp = this;
+}
+
+D3DApp::~D3DApp()
+{
+    if (md3dDevice != nullptr)
+    {
+        FlushCommandQueue();
+    }
+}
+
+LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    return D3DApp::GetApp()->MsgProc(hwnd, msg, wParam, lParam);
+}
+
+void D3DApp::CreateRtvAndDsvDescriptorHeap()
+{
+
+}
+
+void D3DApp::OnResize()
+{
+
+}
+
+bool D3DApp::InitialMainWindow()
+{
+    WNDCLASS wc;
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = MainWndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = mhAppInst;
+    wc.hIcon = LoadIcon(0, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(0, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
+    wc.lpszMenuName = 0;
+    wc.lpszClassName = L"MainWindow";
+
+    if (!RegisterClass(&wc))
+    {
+        MessageBox(0, L"Register class Failed", 0, 0);
+        return false;
+    }
+
+    RECT R = { 0,0,mClientWidth,mClientHeight };
+    AdjustWindowRect(&R, WS_OVERLAPPEDWINDOW, false);
+    int width = R.right - R.left;
+    int height = R.bottom - R.top;
+
+    mhMainWnd = CreateWindow(L"MainWindow", mMainWindowCaption.c_str(), WS_OVERLAPPEDWINDOW,
+                            CW_USEDEFAULT, CW_USEDEFAULT, width, height, 
+                            0, 0, mhAppInst, 0);
+
+    if (mhMainWnd == nullptr)
+    {
+        MessageBox(0, L"Create MainWindow Failed", 0, 0);
+        return false;
+    }
+
+    ShowWindow(mhMainWnd, SW_SHOW);
+    UpdateWindow(mhMainWnd);
+
+    return true;
+}
+
+bool D3DApp::InitialDirect3D()
+{
+    #if defined(DEBUG) || defined(_DEBUG)
+    {
+        Microsoft::WRL::ComPtr<ID3D12Debug> debugController;
+        ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)));
+        debugController->EnableDebugLayer();
+    }
+    #endif
+    
+    //创建IDXGIFactory
+    ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&mdxgiFactory)));
+
+    //尝试创建D3DDevice
+    HRESULT hardwareResult = D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&md3dDevice));
+
+    //若创建失败，回退至WARP设备
+    if (FAILED(hardwareResult))
+    {
+        Microsoft::WRL::ComPtr<IDXGIAdapter> pWarpAdapter;
+        ThrowIfFailed(mdxgiFactory->EnumWarpAdapter(IID_PPV_ARGS(&pWarpAdapter)));
+
+        ThrowIfFailed(D3D12CreateDevice(pWarpAdapter.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&md3dDevice)));
+    }
+
+    //创建Fence
+    ThrowIfFailed(md3dDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&md3dFence)));
+    //获取相应描述符大小
+    mRtvDescriptorSize = md3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+    mDsvDescriptorSize = md3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
+    mCbvSrvUavDescriptorSize = md3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+
+    //检测对4X MSAA的支持
+}
+
+D3DApp* D3DApp::GetApp()
+{
+    return mApp;
+}
+
+HINSTANCE D3DApp::APPInst() const
+{
+    return mhAppInst;
+}
+
+HWND D3DApp::MainWnd() const
+{
+    return mhMainWnd;
+}
+
+float D3DApp::AspectRatio() const
+{
+    return static_cast<float>(mClientWidth) / mClientHeight;
+}
+
+bool D3DApp::Get4xMsaaState() const
+{
+    return m4xMsaaState;
+}
+
+void D3DApp::Set4xMsaaState(bool value)
+{
+    if (m4xMsaaState != value)
+    {
+        m4xMsaaState = value;
+        //修改4XMSAA设置之后要重新创建交换链
+        CreateSwapChain();
+        OnResize();
+    }
+}
+
+int D3DApp::Run()
+{
+    MSG msg = { 0 };
+    
+    mTimer.Reset();
+
+    while (msg.message != WM_QUIT)
+    {
+        if (PeekMessage(&msg,0,0,0,PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            mTimer.Tick();
+
+            if (!mAppPaused)
+            {
+                CalculateFrameStats();
+                Update(mTimer);
+                Draw(mTimer);
+            }
+            else
+            {
+                Sleep(100);
+            }
+        }
+    }
+    return (int)msg.wParam;
+}
+
+bool D3DApp::Initialize()
+{
+
+}
+
+LRESULT D3DApp::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+
+}
+
--- D3D12Render/Common/d3dApp.h
+++ D3D12Render/Common/d3dApp.h
@@ -1,2 +1,147 @@
 ﻿#pragma once
 
+#if defined(DEBUG) || defined(_DEBUG)
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
+
+#include "d3dUtil.h"
+#include "GameTimer.h"
+
+//链接需要的D3D12库
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib,"D3D12.lib")
+#pragma comment(lib,"dxgi.lib")
+
+//单例,只允许全局有一个实例
+
+class D3DApp
+{
+
+protected:
+
+    D3DApp();
+    D3DApp(HINSTANCE hInstance);
+    //不希望D3DApp类被拷贝，故把相关拷贝构造函数与运算符标记为删除
+    D3DApp(const D3DApp& rhs) = delete;
+    D3DApp& operator=(const D3DApp& rhs) = delete;
+    virtual ~D3DApp();
+
+public:
+
+    static D3DApp* GetApp();
+    HINSTANCE APPInst() const;
+    HWND MainWnd() const;
+
+    //宽高比
+    float AspectRatio() const;
+
+    bool Get4xMsaaState() const;
+    void Set4xMsaaState(bool value);
+
+    //消息循环函数
+    int Run();
+
+    virtual bool Initialize();
+    virtual LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+protected:
+    virtual void CreateRtvAndDsvDescriptorHeap();
+    virtual void OnResize();
+    virtual void Update(const GameTimer& gt) = 0;
+    virtual void Draw(const GameTimer& gt) = 0;
+
+    //便于重写鼠标输入消息的处理流程
+    virtual void OnMouseDown(WPARAM btnState, int x, int y) {}
+    virtual void OnMouseUp(WPARAM btnState, int x, int y) {}
+    virtual void OnMouseMove(WPARAM btnState, int x, int y) {}
+
+protected:
+    bool InitialMainWindow();
+    bool InitialDirect3D();
+    //创建命令列表，命令分配器，以及命令队列
+    void CreateCommandObject();
+    //创建交换链
+    void CreateSwapChain();
+
+    void FlushCommandQueue();
+
+    //获取当前后台缓冲区
+    ID3D12Resource* CurrentBackBuffer() const 
+    {
+        return mSwapChainBuffer[mCurrentBackBuffer].Get();
+    }
+
+    //获取当前后台缓冲区描述符
+    D3D12_CPU_DESCRIPTOR_HANDLE CurrentBackBufferView() const
+    {
+        return CD3DX12_CPU_DESCRIPTOR_HANDLE(mRtvHeap->GetCPUDescriptorHandleForHeapStart(), mCurrentBackBuffer,mRtvDescriptorSize);
+    }
+
+    D3D12_CPU_DESCRIPTOR_HANDLE DepthStencilView() const
+    {
+        return mDsvHeap->GetCPUDescriptorHandleForHeapStart();
+    }
+
+    void CalculateFrameStats();
+
+    void LogAdapters();
+    void LogAdapterOutput();
+    void LogOutputDisplayModes(IDXGIOutput* Output, DXGI_FORMAT format);
+
+protected:
+    static D3DApp* mApp;
+
+    HINSTANCE mhAppInst = nullptr;              //应用程序实例句柄
+    HWND mhMainWnd = nullptr;                   //主窗口句柄
+    bool mAppPaused = false;                    //应用程序是否暂停
+    bool mMinimized = false;                    //应用程序是否最小化
+    bool mMaximized = false;                    //应用程序是否最大化
+    bool mResizing = false;                     //
+    bool mFullscreenState = false;              //是否开启全屏模式
+
+    bool m4xMsaaState = false;                  //是否开启4X MSAA
+    UINT m4xMsaaQuality = 0;                    //4X MSAA的质量级别
+
+    GameTimer mTimer;
+
+    Microsoft::WRL::ComPtr<IDXGIFactory4> mdxgiFactory;
+    Microsoft::WRL::ComPtr<IDXGISwapChain> mdxgiSwapChain;
+    Microsoft::WRL::ComPtr<ID3D12Device> md3dDevice;
+
+    Microsoft::WRL::ComPtr<ID3D12Fence> md3dFence;
+    UINT64 mCurrentFence = 0;
+
+    //CommandObject
+    Microsoft::WRL::ComPtr<ID3D12CommandQueue> mCommandQueue;
+    Microsoft::WRL::ComPtr<ID3D12CommandAllocator> mDirectCmdListAlloc;
+    Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> mCommandList;
+
+    static const int SwapChainBufferCount = 2;
+    int mCurrentBackBuffer = 0;
+    Microsoft::WRL::ComPtr<ID3D12Resource> mSwapChainBuffer[SwapChainBufferCount];
+    Microsoft::WRL::ComPtr<ID3D12Resource> mDepthStencilBuffer;
+
+    //缓冲区描述符堆
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> mRtvHeap;      //渲染目标描述符堆
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> mDsvHeap;      //深度/模板缓冲区描述符堆
+
+    //Viewport
+    D3D12_VIEWPORT mScreenViewport;
+
+    //裁剪矩形
+    D3D12_RECT mScissorRect;
+
+    //描述符大小
+    UINT mRtvDescriptorSize = 0;
+    UINT mDsvDescriptorSize = 0;
+    UINT mCbvSrvUavDescriptorSize = 0;
+
+    //下列值应该在派生类的构造函数中自定义
+    std::wstring mMainWindowCaption = L"d3d App";
+    D3D_DRIVER_TYPE md3dDriverType = D3D_DRIVER_TYPE_HARDWARE;
+    DXGI_FORMAT mBackBufferFormat = DXGI_FORMAT_R8G8B8A8_UNORM;             //后台缓冲区数据格式
+    DXGI_FORMAT mDepthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;        //深度/模板缓冲区数据格式
+    int mClientWidth = 800;
+    int mClientHeight = 600;
+};
