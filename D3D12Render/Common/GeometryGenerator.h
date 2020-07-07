19c73fda31c0464070a91a9e6fb840ca87795820
MythGalaxy <405503818@qq.com>
2020-07-07 18:59:28 +0800

    update

--- D3D12Render/Common/GeometryGenerator.h
+++ D3D12Render/Common/GeometryGenerator.h
@@ -19,6 +19,34 @@
 #include <vector>
 
 
+class GeometryGenrator
+{
+public:
+
+    using uint16 = std::uint16_t;
+    using UINT32 = std::uint32_t;
+
+    //定义顶点结构体，内含Position位置、法向量Normal、切向量TangentU、UV纹理坐标四个属性
+    struct Vertex
+    {
+        //构造函数
+        Vertex() {}
+        Vertex(const DirectX::XMFLOAT3& p, const DirectX::XMFLOAT3& n, const DirectX::XMFLOAT3 t, const DirectX::XMFLOAT2 uv): 
+        Position(p),Normal(n),TangentU(t),TexC(uv){}
+        Vertex(
+            float px, float py, float pz,
+            float nx, float ny, float nz,
+            float tx, float ty, float tz,
+            float u, float v) :
+            Position(px, py, pz), Normal(nx, ny, nz), TangentU(tx, ty, tz), TexC(u, v) {}
+
+        DirectX::XMFLOAT3 Position;
+        DirectX::XMFLOAT3 Normal;
+        DirectX::XMFLOAT3 TangentU;
+        DirectX::XMFLOAT2 TexC;
+
+    };
+};
 
 
 //class GeometryGenerator
