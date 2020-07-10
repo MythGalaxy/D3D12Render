06677c7371f21f1d7df12af395a2977b63aa9513
MythGalaxy <405503818@qq.com>
2020-07-10 18:24:47 +0800

    update

--- D3D12Render/Common/GeometryGenerator.h
+++ D3D12Render/Common/GeometryGenerator.h
@@ -24,7 +24,7 @@ class GeometryGenrator
 public:
 
     using uint16 = std::uint16_t;
-    using UINT32 = std::uint32_t;
+    using uint32 = std::uint32_t;
 
     //定义顶点结构体，内含Position位置、法向量Normal、切向量TangentU、UV纹理坐标四个属性
     struct Vertex
@@ -46,6 +46,59 @@ public:
         DirectX::XMFLOAT2 TexC;
 
     };
+
+    //欲构造的几何体Mesh所需的数据结构体
+    struct MeshData
+    {
+        //顶点与索引
+        std::vector<Vertex> Vertices;
+        std::vector<uint32> Indices32;
+
+        std::vector<uint16>& GetIndices16()
+        {
+            if (mIndices16.empty())
+            {
+                mIndices16.resize(Indices32.size());
+                for (size_t i = 0;i != Indices32.size();++i)
+                {
+                    mIndices16[i] = static_cast<uint16>(Indices32[i]);
+                }
+            }
+            return mIndices16;
+        }
+
+    private:
+        std::vector<uint16> mIndices16;
+    };
+
+    //生成长方体Box,中心位于原点
+    MeshData CreateBox(float width, float height, float depth, uint32 numSubdivisions);
+
+    //生成柱体,中心位于原点,stackCount为层数,sliceCount为每一层的顶点数，sliceCount越大，横截面越接近圆形
+    MeshData CreateCylinder(float bottomRadius, float topRadius, float height, uint32 sliceCount,uint32 stackCount);
+
+    //生成球体，中心位于原点，算法与生成柱体算法类似
+    MeshData CreateSphere(float radius, uint32 sliceCount, uint32 stackCount);
+
+    //生成几何球体，中心位于原点，通过对一个正二十面体进行指定次数的曲面细分来完成
+    MeshData CreateGeoSphere(float radius, uint32 numSubDivisions);
+
+    //生成一个由小方格组成的平面，中心在原点,有m行n列
+    MeshData CreateGrid(float width, float depth, uint32 m, uint32 n);
+
+    //生成quad
+    MeshData CreateQuad(float x, float y, float w, float h, float depth);
+
+private:
+    //曲面细分函数
+    void Subdivide(MeshData& meshData);
+
+    //求中点函数，曲面细分函数中调用此函数来获取细分之后新增的顶点的相应数据
+    Vertex MidPoint(const Vertex& v0, const Vertex& v1);
+
+    //柱体的顶面与底面要特殊创建，需要两个函数
+    void BuildCylinderTopCap(float bottomRadius,float topRadius,float height, uint32 sliceCount, uint32 stackCount,MeshData& meshData);
+    void BuildCylinderBottomCap(float bottomRadius, float topRadius, float height, uint32 sliceCount, uint32 stackCount, MeshData& meshData);
 };
 
 
